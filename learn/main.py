d89d919b10ecb498cb037480b3b013d8001e1386
brianwaw <bwaweru408@gmail.com>
2024-12-14 22:24:22 +0300

    added numpy pandas and scipy

--- learn/main.py
+++ learn/main.py
@@ -1,12 +1,29 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4], [1,4,9,16])
+# Firt Plot
+
+# plt.plot([1,2,3,4], [1,4,9,16])
+
+# plt.xlabel('x axis')
+
+# plt.ylabel('y axis')
+
+# plt.title('First plot for me ')
+
+# plt.show()
+
+
+
+
+# Multiple plots on one figure
+
+plt.plot([1,2,3,4], 'r', [10,20,30,40], 'go')
+
+plt.title('multiple plots representation')
 
 plt.xlabel('x axis')
 
 plt.ylabel('y axis')
-
-plt.title('First plot for me ')
 
 plt.show()
 
